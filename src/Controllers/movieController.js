119d3bf20265a416bfbaf1b0292b8d2c9b563d28
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2024-05-09 12:10:12 -0600

    Se agregó la funcionalidad para actualizar una pelicua

--- src/Controllers/movieController.js
+++ src/Controllers/movieController.js
@@ -1,28 +1,70 @@
 "use strict"
 const { connectDB } = require('../../configs/neo4jConfig');
+const {encryptPassword, dencryptPassword, dataObligatory} = require("../utils/validate");
+const neo4j = require('neo4j-driver');
 
-// Método para buscar una película por su nombre
-exports.searchMovie = async (movieName) => {
+
+// Método para buscar una película por su title
+async function findMovie(title) {
     let driver;
     try {
         driver = await connectDB();
         const session = driver.session();
-
+  
         const result = await session.run(
-            'MATCH (m:Movie {name: $movieName}) RETURN m',
-            { movieName: movieName }
+            'MATCH (m:Movie {title: $title}) RETURN m',
+            { title: title }
         );
-
+  
         if (result.records.length > 0) {
-            const movie = result.records[0].get('m').properties;
-            return movie;
+            const user = result.records[0].get('m').properties;
+            return user;
         } else {
-            return res.status(400).send({ message: "Pelicula no encontrada" });
+            return null; // Movie no encontrado
         }
     } catch (error) {
-        console.error('Pelicula no encontrada', error);
-        return { error: "Error al buscar película" };
-    }  
+        console.error('Usuario no encontrado', error);
+        throw error;
+    } finally {
+      if (driver) {
+        await driver.close();
+      }
+    }
+  }
+
+exports.updateMovie = async (req, res) =>{
+    let driver;
+    const idMovie = req.params.id;
+    const paramas =req.body;
+    const data ={
+        title: paramas.title
+    }
+
+    try {
+        // Parámetros requeridos
+        const msg = await dataObligatory(data);
+        if (msg) return res.status(400).send(msg);
+
+        // Verificar si ya existe una película con el mismo título
+        const existingMovie = await findMovie(data.title);
+        if (existingMovie) {
+            return res.status(400).send({ message: "El titulo de la pelicula ya existe" });
+        }
+
+        driver = await connectDB();
+        const session = driver.session();
+        const result = await session.run(
+            'MATCH (m:Movie) WHERE id(m) = $idMovie SET m.title = $title RETURN m',
+            { idMovie: neo4j.int(idMovie), title: data.title }
+        );
+
+        return res.status(200).json({ message: "Pelicula actualizada correctamente"});
+        
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Error interno del servidor' });
+    }
     finally {
         if (driver) {
           await driver.close();
--- src/Routes/movieRoutes.js
+++ src/Routes/movieRoutes.js
@@ -4,7 +4,9 @@ const express = require('express');
 const api = express.Router();
 const movieController = require("../Controllers/movieController")
 
-api.get("/searchMovie", movieController.searchMovie);
+//api.get("/searchMovie", movieController.searchMovie);
+api.put("/updateMovie/:id", movieController.updateMovie);
+
 
 
 module.exports = api;
