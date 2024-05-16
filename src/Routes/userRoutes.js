54282dd7e067be34f4cd9926e8f43732fb5d79a6
jruiz002 <josegrg04@gmail.com>
2024-05-16 11:53:32 -0600

    Se modificó una ruta

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -4,8 +4,8 @@ const express = require('express');
 const api = express.Router();
 const userController = require("../Controllers/userController");
 
-api.get("/register", userController.register);
-api.get("/login", userController.login);
+api.post("/register", userController.register);
+api.post("/login", userController.login);
 
 
 module.exports = api;
